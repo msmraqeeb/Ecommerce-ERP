ee67422e74cbe923b4f296209632202b0880ea4f
Shakil Mahmud <shakil.regnum@gmail.com>
2025-10-30 10:21:12 +0000

    Try fixing this error: `Console Error: AxiosError: Request failed with s

--- src/lib/woocommerce.ts
+++ src/lib/woocommerce.ts
@@ -11,7 +11,12 @@ const api = new WooCommerceRestApi({
   url: process.env.WOOCOMMERCE_API_URL || '',
   consumerKey: process.env.WOOCOMMERCE_CONSUMER_KEY || '',
   consumerSecret: process.env.WOOCOMMERCE_CONSUMER_SECRET || '',
-  version: "wc/v3"
+  version: "wc/v3",
+  axiosConfig: {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+  }
 });
 
 export async function getProducts() {
