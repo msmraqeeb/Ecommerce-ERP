f2c32c720aa508e3a4b27bf83a5d66a7f89facde
Shakil Mahmud <shakil.regnum@gmail.com>
2025-10-30 10:39:55 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'kidsparadise.com.bd',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
