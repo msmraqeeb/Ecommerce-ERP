f659d0b5467e11cfc81de29fb3e4cf5d4962773d
Shakil Mahmud <shakil.regnum@gmail.com>
2025-11-01 11:29:18 +0000

    Try fixing this error: `Runtime Error: TypeError: Invalid time period fo

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -17,7 +17,7 @@ export async function encrypt(payload: any) {
   return await new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256' })
     .setIssuedAt()
-    .setExpirationTime('1 day from now')
+    .setExpirationTime('1d')
     .sign(key);
 }
 
