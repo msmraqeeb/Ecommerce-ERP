8e54864ea58b1172692c5196cfc874abf9a8a531
Shakil Mahmud <shakil.regnum@gmail.com>
2025-11-01 05:17:44 +0000

    sku search is not working in right way. fix this error

--- src/lib/woocommerce.ts
+++ src/lib/woocommerce.ts
@@ -53,17 +53,31 @@ export async function getProducts({
     if (order) {
       params.order = order;
     }
+    
+    let searchPromise = Promise.resolve({ data: [], headers: {} });
+    let skuPromise = Promise.resolve({ data: [], headers: {} });
+
     if (search) {
-      params.search = search;
+      searchPromise = api.get("products", { ...params, search });
+      skuPromise = api.get("products", { ...params, sku: search });
+    } else {
+      searchPromise = api.get("products", params);
     }
     
-    const response = await api.get("products", params);
-    const products: Product[] = response.data;
-    const totalPages = Number(response.headers['x-wp-totalpages']);
-    const totalProducts = Number(response.headers['x-wp-total']);
+    const [searchResponse, skuResponse] = await Promise.all([searchPromise, skuPromise]);
+    
+    const combinedProducts = [...searchResponse.data, ...skuResponse.data];
+    const uniqueProducts = Array.from(new Map(combinedProducts.map(p => [p.id, p])).values());
+
+    // For simplicity, we'll use the headers from the main search response for pagination.
+    // This might not be perfectly accurate if the SKU search returns vastly different total numbers,
+    // but it's a reasonable trade-off to avoid complex header merging.
+    const finalResponse = search && search.length > 0 ? searchResponse : await api.get("products", { per_page: 1, page });
+    const totalPages = Number(finalResponse.headers['x-wp-totalpages']);
+    const totalProducts = Number(finalResponse.headers['x-wp-total']);
 
     return {
-      products,
+      products: uniqueProducts,
       totalPages,
       totalProducts,
     };
